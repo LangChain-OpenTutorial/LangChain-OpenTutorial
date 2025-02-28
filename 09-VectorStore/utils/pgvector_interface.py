9b44d9412ce658450bdc135d4d3d3f7185ba87f3
Lee Jongho <kumathmatics@gmail.com>
2025-02-28 17:48:36 +0900

    interface update

--- 09-VectorStore/utils/pgvector_interface.py
+++ 09-VectorStore/utils/pgvector_interface.py
@@ -4,26 +4,9 @@ from typing import List, Union, Dict, Any, Optional, Iterable
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from tqdm.auto import tqdm
 from hashlib import md5
-import os, time, uuid
-import contextlib
-import psycopg
-import sqlalchemy
-from sqlalchemy import SQLColumnExpression, cast, create_engine, delete, func, select
-from sqlalchemy.dialects.postgresql import JSON, JSONB, JSONPATH, UUID, insert
-from sqlalchemy.engine import Connection, Engine
-from sqlalchemy.ext.asyncio import (
-    AsyncEngine,
-    AsyncSession,
-    async_sessionmaker,
-    create_async_engine,
-)
-from sqlalchemy.orm import (
-    Session,
-    declarative_base,
-    relationship,
-    scoped_session,
-    sessionmaker,
-)
+import os, time, uuid, json
+
+import psycopg2
 from typing import (
     Any,
     AsyncGenerator,
@@ -38,118 +21,15 @@ from typing import (
     Type,
     Union,
 )
-from typing import (
-    cast as typing_cast,
-)
 
-Base = declarative_base()
-
-
-def _create_vector_extension(conn: Connection) -> None:
-    statement = sqlalchemy.text(
-        "SELECT pg_advisory_xact_lock(1573678846307946496);"
-        "CREATE EXTENSION IF NOT EXISTS vector;"
-    )
-    conn.execute(statement)
-    conn.commit()
-
-
-def _get_embedding_collection_store(vector_dimension: Optional[int] = None) -> Any:
-    global _classes
-    if _classes is not None:
-        return _classes
-
-    from pgvector.sqlalchemy import Vector  # type: ignore
-
-    class CollectionStore(Base):
-        """Collection store."""
-
-        __tablename__ = "langchain_pg_collection"
-
-        uuid = sqlalchemy.Column(
-            UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
-        )
-        name = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
-        cmetadata = sqlalchemy.Column(JSON)
-
-        embeddings = relationship(
-            "EmbeddingStore",
-            back_populates="collection",
-            passive_deletes=True,
-        )
-
-        @classmethod
-        def get_by_name(
-            cls, session: Session, name: str
-        ) -> Optional["CollectionStore"]:
-            return (
-                session.query(cls)
-                .filter(typing_cast(sqlalchemy.Column, cls.name) == name)
-                .first()
-            )
-
-        @classmethod
-        def get_or_create(
-            cls,
-            session: Session,
-            name: str,
-            cmetadata: Optional[dict] = None,
-        ) -> Tuple["CollectionStore", bool]:
-            """Get or create a collection.
-            Returns:
-                 Where the bool is True if the collection was created.
-            """  # noqa: E501
-            created = False
-            collection = cls.get_by_name(session, name)
-            if collection:
-                return collection, created
-
-            collection = cls(name=name, cmetadata=cmetadata)
-            session.add(collection)
-            session.commit()
-            created = True
-            return collection, created
-
-    class EmbeddingStore(Base):
-        """Embedding store."""
-
-        __tablename__ = "langchain_pg_embedding"
-
-        id = sqlalchemy.Column(
-            sqlalchemy.String, nullable=True, primary_key=True, index=True, unique=True
-        )
-
-        collection_id = sqlalchemy.Column(
-            UUID(as_uuid=True),
-            sqlalchemy.ForeignKey(
-                f"{CollectionStore.__tablename__}.uuid",
-                ondelete="CASCADE",
-            ),
-        )
-        collection = relationship(CollectionStore, back_populates="embeddings")
-
-        embedding: Vector = sqlalchemy.Column(Vector(vector_dimension))
-        document = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-        cmetadata = sqlalchemy.Column(JSONB, nullable=True)
-
-        __table_args__ = (
-            sqlalchemy.Index(
-                "ix_cmetadata_gin",
-                "cmetadata",
-                postgresql_using="gin",
-                postgresql_ops={"cmetadata": "jsonb_path_ops"},
-            ),
-        )
-
-    _classes = (EmbeddingStore, CollectionStore)
-
-    return _classes
-
-
+DISTANCE = {
+    "cosine": "<=>",
+    "l2": "<->",
+    "l1": '<+>',
+}
 class pgVectorIndexManager:
     def __init__(
         self,
-        connection_str=None,
         host=None,
         port=None,
         username=None,
@@ -157,122 +37,244 @@ class pgVectorIndexManager:
         password=None,
         passwd=None,
         dbname=None,
-        db=None,
-        collection_name=None,
-        create_extension=True,
-        dimension=None,
-        pre_delete_collection=False,
+        db=None
     ):
-        if connection_str is not None:
-            self.connection_str = connection_str
-        else:
-            assert host is not None, "host is missing"
-            assert port is not None, "port is missing"
-            assert (
-                username is not None or user is not None
-            ), "username(or user) is missing"
-            assert (
-                password is not None or passwd is not None
-            ), "password(or passwd) is missing"
-            assert dbname is not None or db is not None, "dbname(or db) is missing"
+        assert host is not None, "host is missing"
+        assert port is not None, "port is missing"
+        assert (
+            username is not None or user is not None
+        ), "username(or user) is missing"
+        assert (
+            password is not None or passwd is not None
+        ), "password(or passwd) is missing"
+        assert dbname is not None or db is not None, "dbname(or db) is missing"
 
-            self.host = host
-            self.port = port
-            self.userName = username if username is not None else user
-            self.passWord = password if password is not None else passwd
-            self.dbName = dbname if dbname is not None else db
+        self.host = host
+        self.port = port
+        self.userName = username if username is not None else user
+        self.passWord = password if password is not None else passwd
+        self.dbName = dbname if dbname is not None else db
 
-            self.connection_str = (
-                "postgresql+psycopg://{userName}:{passWord}@{host}:{port}/{dbName}"
-            )
-
-        self._engine = create_engine(url=self.connection_str, **{})
-        self.collection_name = collection_name
-        self.use_jsonb = True
-        self.create_extension = create_extension
-        self.dimension = dimension
-        self.pre_delete_collection = pre_delete_collection
-        self.__post_init__()
-        self.session_maker = scoped_session(sessionmaker(bind=self._engine))
-
-    def __post_init__(self):
-        if self.create_extension:
-            self.create_vector_extension()
-
-        EmbeddingStore, CollectionStore = _get_embedding_collection_store(
-            self.dimension
-        )
-
-        self.CollectionStore = CollectionStore
-        self.EmbeddingStore = EmbeddingStore
-
-        self.create_tables_if_not_exists()
-        self.create_collection()
-
-    def create_tables_if_not_exists(self) -> None:
-        with self._make_sync_session() as session:
-            Base.metadata.create_all(session.get_bind())
-            session.commit()
-
-    def create_collection(self) -> None:
-        if self.pre_delete_collection:
-            self.delete_collection()
-        with self._make_sync_session() as session:
-            self.CollectionStore.get_or_create(
-                session, self.collection_name, cmetadata=self.collection_metadata
-            )
-            session.commit()
-
-    @contextlib.contextmanager
-    def _make_sync_session(self) -> Generator[Session, None, None]:
-        """Make an async session."""
-        if self.async_mode:
-            raise ValueError(
-                "Attempting to use a sync method in when async mode is turned on. "
-                "Please use the corresponding async method instead."
-            )
-        with self.session_maker() as session:
-            yield typing_cast(Session, session)
-
-    def create_vector_extension(self) -> None:
-        assert self._engine, "engine not found"
-        try:
-            with self._engine.connect() as conn:
-                _create_vector_extension(conn)
-        except Exception as e:
-            raise Exception(f"Failed to create vector extension: {e}") from e
+    def _connect(self):
+        return psycopg2.connect(database=self.dbName, user=self.userName, password=self.passWord, port=self.port, host=self.host)
 
     def list_indexes(self):
-        pass
+        query = """
+        SELECT table_name 
+        FROM information_schema.tables 
+        WHERE table_schema = 'public'
+        AND table_type = 'BASE TABLE';
+        """
+        
+        try:
+            with self._connect() as conn:
+                cur = conn.cursor()
+                cur.execute(query)
+                
+        except Exception as e:
+            msg = f"List collection failed due to {type(e)} {str(e)}"
+        else:
+            collections = cur.fetchall()
+            msg = ""
+        finally:
+            conn.close()
+            print(msg)
+            return collections
 
-    def delete_index(self, index_name):
-        pass
+    def delete_index(self, collection_name):
+        query = f"DROP TABLE {collection_name};"
+        try:
+            with self._connect() as conn:
+                cur = conn.cursor()
+                cur.execute(query)
+                conn.commit()
+        except Exception as e:
+            flag = False
+            msg = f"Delete collection {collection_name} failed due to {type(e)} {str(e)}"
+        else:
+            flag = True
+            msg = f"Deleted collection {collection_name} successfully."
+        finally:
+            conn.close()
+            print(msg)            
+            return flag
 
-    def create_index(self):
-        pass
+    def _check_extension(self):
+        query = "SELECT * FROM pg_extension;"
+        create_ext_query = "CREATE EXTENSION vector;"
 
-    def get_index(self, index_name: str) -> Dict:
-        pass
+        try:
+            with self._connect() as conn:
+                cur = conn.cursor()
+                cur.execute(query)
+                extensions = str(cur.fetchall())
+        finally:
+            conn.close()
 
+        if 'vector' not in extensions:
+            try:
+                with self._connect() as conn:
+                    cur = conn.cursor()
+                    cur.execute(create_ext_query)
+                    conn.commit()
+            finally:
+                conn.close()
+            
+    def get_index(self, collection_name, embedding):
+        connection_info = {"host": self.host, "port": self.port, "user": self.userName, "password": self.passWord, "dbname": self.dbName}
+        return pgVectorDocumentManager(connection_info=connection_info, collection_name=collection_name, embedding=embedding)
+
+    def create_index(self, collection_name, dimension=None, embedding=None):
+        self._check_extension()
+
+        assert embedding is not None or dimension is not None, "One of embedding or dimension must be provided"
+
+        if dimension is None:
+            dimension = len(embedding.embed_query("foo"))
+
+        query = (
+            f"CREATE TABLE {collection_name} "
+            f"(id bigserial PRIMARY KEY, "
+            f"embedding vector({dimension}) NOT NULL,"
+            "metadata jsonb NULL, "
+            "doc_id text NOT NULL UNIQUE)"
+            )
+        try:
+            with self._connect() as conn:
+                cur = conn.cursor()
+                cur.execute(query)
+                conn.commit()
+        except Exception as e:
+            msg = f"Create collection {collection_name} failed due to {type(e)} {str(e)}"
+            flag = False
+        else:
+            msg = f"Created collection {collection_name} successfully"
+            flag = True
+        finally:
+            print(msg)
+            conn.close()
+            return flag
 
 class pgVectorDocumentManager(DocumentManager):
-    def __init__(self, client):
-        pass
+    def __init__(self, connection_info, collection_name, embedding, distance='cosine'):
+        self.collection_name = collection_name
+        self.connection_info = connection_info
+        self.embedding = embedding
+        self.distance = distance.lower()
 
-    def get_index_info(self):
-        pass
+    def _connect(self):
+        return psycopg2.connect(**self.connection_info)
 
     def _embed_doc(self, texts) -> List[float]:
-        pass
+        embedded = self.embedding.embed_documents(texts)
+        return embedded
 
-    def upsert(self):
-        pass
+    def upsert(
+        self,
+        texts,
+        metadatas=None,
+        ids=None,
+        **kwargs):
+        if ids is not None:
+            assert len(ids) == len(texts), "The length of ids and texts must be the same."
 
-    def upsert_parallel(self):
-        pass
+        elif ids is None:
+            ids = [md5(text.lower().encode("utf-8")).hexdigest() for text in texts]
 
-    def search(self, query, k=10, **kwargs):
-        pass
+        embeds = self._embed_doc(texts)
+
+        params = [
+            (json.dumps(embed), json.dumps(metadata) if metadata else "", doc_id, json.dumps(metadata) if metadata else "") for embed, metadata, doc_id, metadata in zip(embeds, metadatas, ids, metadatas)
+        ]
+
+        query = (
+            f"INSERT INTO {self.collection_name} "
+            "(embedding, metadata, doc_id) "
+            "VALUES (%s,%s,%s) "
+            "ON CONFLICT(doc_id) "
+            "DO UPDATE "
+            "SET metadata=%s"
+
+        )
+
+        try:
+            with self._connect() as conn:
+                cur = conn.cursor()
+                cur.executemany(query, params)
+        except Exception as e:
+            msg = f"Error occured during upsert documents due to {type(e)} {str(e)}"
+            conn.rollback()
+        else:
+            conn.commit()
+            msg = f"Upsert successful"
+        finally:
+            print(msg)
+            conn.close()
+            return ids
+
+    def upsert_parallel(self, texts, metadatas, ids, batch_size=32, workers=10, **kwargs):
+        if ids is not None:
+            assert len(ids) == len(texts), "Size of documents and ids must be the same"
+
+        elif ids is None:
+            ids = [md5(text.lower().encode("utf-8")).hexdigest() for text in texts]
+
+        if batch_size > len(texts):
+            batch_size = len(texts)
+
+        text_batches = [
+            texts[i : i + batch_size] for i in range(0, len(texts), batch_size)
+        ]
+
+        id_batches = [ids[i : i + batch_size] for i in range(0, len(texts), batch_size)]
+
+        meta_batches = [
+            metadatas[i : i + batch_size] for i in range(0, len(texts), batch_size)
+        ]
+
+        with ThreadPoolExecutor(max_workers=workers) as exe:
+            futures = [
+                exe.submit(
+                    self.upsert, texts=text_batch, metadatas=meta_batch, ids=id_batch
+                )
+                for text_batch, meta_batch, id_batch in zip(
+                    text_batches, meta_batches, id_batches
+                )
+            ]
+            results = []
+
+            for future in as_completed(futures):
+                result = future.result()
+                if result:
+                    results.extend(result)
+
+        return results
+
+    def search(self, query, k=10, distance='cosine', **kwargs):
+        self.distance = distance.lower()
+        embeded_query = json.dumps(self.embedding.embed_query(query))
+        search_query = (
+            f"SELECT doc_id, metadata, 1-(embedding {DISTANCE[self.distance]} %s) FROM {self.collection_name} "
+            f"ORDER BY embedding {DISTANCE[self.distance]} %s;"
+        )
+        try:
+            with self._connect() as conn:
+                cur = conn.cursor()
+                cur.execute(search_query, (embeded_query,embeded_query))
+        except Exception as e:
+            print(f"Search failed due to {type(e)} {str(e)}")
+            conn.rollback()
+        else:
+            _result = cur.fetchall()
+            result = list()
+
+            for _r in _result:
+                result.append(
+                    {"doc_id":_r[0], "metadata": _r[1], "score": _r[2]}
+                )
+        finally:
+            conn.close()
+            return result
 
     def delete(self, ids=None, filters=None, **kwargs):
         pass
